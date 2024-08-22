e260cfa544d88fc1cc2103f0dcff35e4c62f3c8b
vicgray <victorygray59@gmail.com>
2024-08-22 11:10:36 -0700

    changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ControllersModule } from './controllers/controllers.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [UsersModule, ControllersModule],
+  imports: [UsersModule, ControllersModule, CommonModule],
 })
 export class AppModule {}
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,23 @@
+import { IsString, IsInt, MaxLength, MinLength } from 'class-validator';
+
+
 export class CreateUserDto {
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   readonly  name:string;
+
+
+  @IsString()
   readonly phone:string;
+
+
+  @IsString()
   readonly email:string;
+
+
+  @IsString()
   readonly  password:string;
 }
+
